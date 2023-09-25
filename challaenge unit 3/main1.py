a9bd8b594c4b0be02d9d0452d09c047b706ba7ba
anm12222cs016 <144413893+anm12222cs016@users.noreply.github.com>
2023-09-25 12:31:09 +0530

    Update and rename main1.py.py to main1.py

--- challaenge unit 3/main1.py
+++ challaenge unit 3/main1.py
@@ -4,7 +4,7 @@ def linear_search_product(product_list, target_product):
         if product == target_product:
             indices.append(i)
     return indices
-products = ["Apple", "Banana", "Orange", "Grapes", "Apple", "Grapes","Apple"]
+products = ["Apple", "banana", "apple","Orange", "Grapes", "Apple", "Grapes","Apple"]
 target = "Apple"
 result = linear_search_product(products, target)
 if result:
